ea9de2891184322a0571a665a98357a143f0c251
Ayush Jef <www.jefayush@gmail.com>
2022-05-18 22:44:55 +0530

    initial commit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
-}); 
+});
 
 userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] } );
 
