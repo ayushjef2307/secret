cf87225cff960e827088c91772c8b79d290236a1
Ayush Jef <www.jefayush@gmail.com>
2022-05-18 23:30:45 +0530

    Step 3: Hashing (md5)

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
-console.log(process.env.API_KEY);
+
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,7 +18,6 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] } );
 
 const User = new mongoose.model("User", userSchema);
 
@@ -35,7 +34,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function (err) {
@@ -55,7 +54,7 @@ app.post("/login", function (req, res) {
         if(err) console.log(err);
         else{
             if (foundUser) {
-                if (foundUser.password === req.body.password) {
+                if (foundUser.password === md5(req.body.password) ) {
                     res.render("secrets");
                 }
             }
